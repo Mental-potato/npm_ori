ccac1d8fbbbcf7df337084089eec634449b35b1b
Mental-potato <153oorrii153@gmail.com>
2025-03-12 13:19:09 +0200

    ts and complie

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// index.ts
+const message = "Hello, TypeScript!";
+console.log(message);
+const addNumbers = (a, b) => {
+    return a + b;
+};
+console.log("Sum:", addNumbers(5, 10));
---
+++ index.ts
@@ -0,0 +1,9 @@
+// index.ts
+const message: string = "Hello, TypeScript!";
+console.log(message);
+
+const addNumbers = (a: number, b: number): number => {
+    return a + b;
+};
+
+console.log("Sum:", addNumbers(5, 10));
